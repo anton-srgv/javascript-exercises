bdfe88b00a63e280429f7272d7ed44b9897bfa77
Antony <43699659+anton-srgv@users.noreply.github.com>
2025-04-26 22:46:37 +0300

    Pass 2 test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  let result = '';
+  if (num < 0) {
+    result = 'ERROR';
+  } else {
+    for (let x = 0; x < num; x++) {
+      result += string;
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
